10cd43fac37b595aa69e8acb6ad9dccde9bfd119
anjali7604 <anjaliandng@gmail.com>
2015-06-17 00:03:15 +0530

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$conn=mysqli_connect("localhost","root","","delta");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo " ";
+?>
+</body>
+</html>
