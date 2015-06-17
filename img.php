fa6b3037f1a7ed9d5bb58f33149064fb804c1691
anjali7604 <anjaliandng@gmail.com>
2015-06-17 22:34:16 +0530

    Update and rename README.md to img.php

---
+++ img.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+
+
+
+
+<?php 
+
+session_start();
+include("conn.php"); 
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET")
+
+
+{
+    $iquery="SELECT * FROM reg;";
+    $iresult=mysqli_query($conn,$iquery);
+
+		while($row=mysqli_fetch_array($iresult))
+		{
+			
+		     echo "<br><a href=\"".$row['location']."\">".$row['roll']."</a><br>";
+		}
+
+	
+	mysqli_close($conn);
+	
+}
+
+
+
+
+
+?>
+
+
+<form name="display"  method="get" action="<?php echo $_SERVER["PHP_SELF"];?>">
+
+
+
+
+
+</form>
+
+</body>
+</html>
