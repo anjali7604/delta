9c18bec81b14f54ad8e2b626cb8f5db74b7a7e3b
anjali7604 <anjaliandng@gmail.com>
2015-07-16 14:39:37 +0530

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Factory Inspection Department</title>
+ 
+ <style>
+ #contact,p.tel{
+text-align:center;
+margin-top:5%;
+color:red;}
+ 
+ #image{float:left;
+ margin-left:75%;
+ margin-top:1%;}
+ 
+ #top{background-color: #b0c4de;}
+ 
+ #logo,#page{float:left;}
+ 
+ #header{
+float:left;
+font-size:60px;
+margin-top:80px;
+
+}
+ </style>
+ 
+ 
+ 
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="theme/css/bootstrap.min.css" rel="stylesheet">
+ <link href="theme/css/sb-admin-2.css" rel="stylesheet">
+ <script src="theme/js/html5shiv.js"></script>
+ <script src="theme/js/respond.min.js"></script>
+ <script src="theme/js/jquery-1.11.0.js"></script>
+ </head>
+
+
+
+
+
+
+
+
+<body>
+<nav class="navbar navbar-inverse" role="navigation">
+ <div id="top"><img src="logo.png" id="logo">
+ 
+ <h1 id="header"> FACTORY INSPECTION DEPARTMENT</h1><br><br><br><br><br><br><br><br><br><br><br>
+ </div>
+ <div id="page">
+ <ul class="nav navbar-nav">
+ <li ><a href="home.php">Home</a></li>
+ <li><a href="acts.php">Policies and Acts</a></li>
+ <li><a href="survey.php">Survey reports</a></li>
+ <li class="active"><a href="contactus.php">Contact Us</a></li>
+ </ul>
+ </li>
+ </ul>
+ </div>
+ </nav>
+ <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+
+ <li>
+<a href="authorised.php"><i class="fa fa-dashboard fa-fw"></i> Authorised Factories</a>
+</li>
+<li>
+<a href="unauthorised.php"><i class="fa fa-dashboard fa-fw"></i> Unauthorised Factories</a>
+</li>
+<li>
+<a href="faq.php"><i class="fa fa-dashboard fa-fw"></i> FAQ</a>
+</li>
+
+            </ul>
+        </div>
+      </div>
+	  
+	
+
+<h1 id="contact">ContactUs</h1>
+<p class="tel">
+Factories Inspection Department<br>
+Govt. of India<br>
+6-c Jhalana Institutional Area<br>
+Behind Commerce College<br>
+ JLN Marg, New Delhi
+<br>
+<b>Tel:</b>0141-2709659, 2709616
+<br>
+<b>Fax:</b>0141-2709659<br>
+<b>Email id:</b>cifb_hqjpr@rediffmail.com, cifb.jpr-rj@nic.in
+
+	
+</body>
+</html>
