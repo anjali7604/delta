6986b7cb0e2d22a747312b070cb80c00f22369f9
anjali7604 <anjaliandng@gmail.com>
2015-07-16 14:40:51 +0530

    Create authorised.php

---
+++ authorised.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Factory Inspection Department</title>
+ 
+ <style>
+ #autho{float:left;
+ margin-left:30%;
+ margin-top:5%;}
+ #obj{
+ margin-left:5%;
+ color:green;
+ font-size:20px;}
+ 
+ #image{float:left;
+ margin-left:75%;
+ margin-top:1%;}
+ 
+ #top{background-color: #b0c4de;}
+ 
+ #logo,#page{float:left;}
+ 
+ #header{
+float:left;
+font-size:60px;
+margin-top:80px;
+
+}
+ </style>
+ 
+ 
+ 
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="theme/css/bootstrap.min.css" rel="stylesheet">
+ <link href="theme/css/sb-admin-2.css" rel="stylesheet">
+ <script src="theme/js/html5shiv.js"></script>
+ <script src="theme/js/respond.min.js"></script>
+ <script src="theme/js/jquery-1.11.0.js"></script>
+ </head>
+
+
+
+
+
+
+
+
+<body>
+<nav class="navbar navbar-inverse" role="navigation">
+ <div id="top"><img src="logo.png" id="logo">
+ 
+ <h1 id="header">FACTORY INSPECTION DEPARTMENT</h1><br><br><br><br><br><br><br><br><br><br><br>
+ </div>
+ <div id="page">
+ <ul class="nav navbar-nav">
+ <li><a href="home.php">Home</a></li>
+ <li><a href="acts.php">Policies and Acts</a></li>
+ <li><a href="survey.php">Survey reports</a></li>
+ <li><a href="contactus.php">Contact Us</a></li>
+ </ul>
+ </li>
+ </ul>
+ </div>
+ </nav>
+ <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+
+ <li class="active">
+<a href="authorised.php"><i class="fa fa-dashboard fa-fw"></i> Authorised Factories</a>
+</li>
+<li>
+<a href="unauthorised.php"><i class="fa fa-dashboard fa-fw"></i> Unauthorised Factories</a>
+</li>
+<li>
+<a href="faq.php"><i class="fa fa-dashboard fa-fw"></i> FAQ</a>
+</li>
+
+            </ul>
+        </div>
+      </div>
+	  
+	  
+	  
+	  
+	  
+	  <div id="autho">
+	  
+	  <b><h2>Authorised Factories</h2></b>
+	  <ol>
+	  
+	 <li> M/s. AKS Expo-Chem Pvt. Ltd. 524-
+526, Somdutt Chambers-II, 9, Bhikaji
+Cama Place, New Delhi - 110066</li>
+</li>
+
+
+
+<li>M/s. AMA Industries Pvt. Ltd.,
+Maimoon Chambers, Gandhibagh,
+Nagpur – 440 032.
+</li>
+
+
+
+
+<li>M/s. Bharatiya Barood Udyog, Sanyal
+House, Op. to Corpn Park, Gandhi
+Chowk, Sadar, Nagpur -01
+</li>
+
+
+
+
+
+
+
+
+<li>M/s. Bharat Explosives Ltd., 9K.M.
+Lalitpur Jhansi Road, Lalitpur – 284
+403. (U.P)
+</li>
+
+
+
+
+<li>
+M/s Milk Products Factory, A.P. Dairy Development Co-op Federation Ltd, Lalapet, Hyderabad-500 017
+</li>
+
+
+
+
+
+
+
+
+
+
+<li>
+M/s Upadrasta Dairy Specialities Pvt Ltd., 176-2-3, Plot No:12, Tarbund Cross road, Secunderabad
+</li>
+
+
+<li>
+
+M/s Seetharam Ramgopal Malani, 14-6-66, Begum Bazaar, Hyderabad-500 012
+
+</li>
+
+<li>
+M/s Seetharam Ramgopal Malani, 14-6-66, Begum Bazaar, Hyderabad-500 012
+</li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<li>
+M/s Heritage foods (India) Ltd., on NH-9, Cheruvugattu village,Narketpally Mandal, Nalgonda Dist, Pin: 508 254</li>
+	  
+	  </ol>
+	  
+	 
+	  </div>
+  
+</body>
+</html>
