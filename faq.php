76743673a37c8b035b58b521ba89c310439ae3a9
anjali7604 <anjaliandng@gmail.com>
2015-07-16 14:42:11 +0530

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Factory Inspection Department</title>
+ 
+ 
+ <style>
+ #faq{float:left;
+ margin-left:20%;
+ }
+ 
+ #image{float:left;
+ margin-left:75%;
+ margin-top:1%;}
+ 
+ #top{background-color: #b0c4de;}
+ 
+ #logo,#page{float:left;}
+ 
+ #header{
+float:left;
+font-size:60px;
+margin-top:80px;
+
+}
+ </style>
+ 
+ 
+ 
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="theme/css/bootstrap.min.css" rel="stylesheet">
+ <link href="theme/css/sb-admin-2.css" rel="stylesheet">
+ <script src="theme/js/html5shiv.js"></script>
+ <script src="theme/js/respond.min.js"></script>
+ <script src="theme/js/jquery-1.11.0.js"></script>
+ </head>
+
+
+
+
+
+
+
+
+<body>
+<nav class="navbar navbar-inverse" role="navigation">
+ <div id="top"><img src="logo.png" id="logo">
+ 
+ <h1 id="header">FACTORY INSPECTION DEPARTMENT</h1><br><br><br><br><br><br><br><br><br><br><br>
+ </div>
+ <div id="page">
+ <ul class="nav navbar-nav">
+ <li ><a href="home.php">Home</a></li>
+ <li><a href="acts.php">Policies and Acts</a></li>
+ <li><a href="survey.php">Survey reports</a></li>
+ <li><a href="contactus.php">Contact Us</a></li>
+ </ul>
+ </li>
+ </ul>
+ </div>
+ </nav>
+ <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+
+ <li>
+<a href="authorised.php"><i class="fa fa-dashboard fa-fw"></i> Authorised Factories</a>
+</li>
+<li>
+<a href="unauthorised.php"><i class="fa fa-dashboard fa-fw"></i> Unauthorised Factories</a>
+</li>
+<li class="active">
+<a href="faq.php"><i class="fa fa-dashboard fa-fw"></i> FAQ</a>
+</li>
+
+            </ul>
+        </div>
+      </div>
+	  
+	  
+	
+
+
+<div id="faq">
+<h2>The procedure/ guidelines for submitting an application for registration of a  new factory.</h2>
+The following documents should be submitted to the office of the Chief Inspector of Factories for registration of a new factory:
+<ul>
+<li>  Form No.2 (Annexure -2) in triplicate, filled in completely and duly signed/stamped by the Occupier and Manager of the Factory. The number of workers proposed to be employed, connected power load, manufacturing process etc. should be filled in at all the appropriate places. The Registration fee as per enclosed Fee Schedule ( A & B) (Annexure -3) should be deposited in the State Treasury. or an A/C payees Bank demand draft favoring The Chief Inspector of Factories & Boilers, Rajasthan, payable at Jaipur should be sent along with the Application Form ( Form No.2). The registration fee is as per enclosed fee schedule with additional 50% fee payable at the time of registration</li>
+
+<li> At Sr. No. 8 of the Form No.2, complete name, fathers name, residential address of the owner/occupier/proprietor/partner/director or manufacturing Agent, as the case may be should be clearly stated. In case of a private or a public limited company, only one of the directors of the company can be named/appointed a the Occupier of the factory.</li>
+
+<li> The attested/notorized proof of ownership/possession of the land and the building where the factory is proposed to be started, must be enclosed.</li>
+
+<li> In case of a public or private limited company, three self attested/notarized copies of the Memorandum and Articles of Association must be enclosed. If there has been any change in the directors after incorporation of the company, attested copies of Board Resolution and Form No.32 submitted to the Registrar of Companies must be enclosed, along with a duly signed/attested copies of latest list of directors. DIN of the directors should also be furnished.</li>
+
+<li> Before commencing any manufacturing process in any building/shed inside the factory premises, a building stability certificate must be obtained from a recognized/approved competent person and submitted to the Chief Inspector of Factories, as required under Rules 3C  of the Rajasthan Factories Rules 1951.</li>
+</ul>
+
+<h2>The procedure/ guidelines for submitting an application for renewal of factory licence.</h2>
+<ol>
+
+<li>Application complete in all respects in the prescribed Form No.2 in triplicate with the prescribed Fee along with the original license must reach the office of the Chief Inspector of Factories and Boilers, Rajasthan, Jaipur or the Regional Dy. Chief /Senior/Inspector of Factories and Boilers, positively before 31st January. Late fee at the rate of 25% of the license renewal fee is payable on all applications received after the 31st January. In case 31st January happens to be a Sunday or a Gazetted holiday, the next working day shall be treated as the last date of receiving the license renewal applications.
+In case there is any amendment in the constitution of the factory/ company/firm its ownership, name of the factory manufacturing process, change in the employment of number of workers, or the power being taken into use, a revised application for amendment of the factory license shall have to be submitted along with an amendment fee of Rs.100/- in addition to the difference of fee if payable.
+</li><li>The Original license must be enclosed with the application for license renewal, or a fee of Rs.100/- must be deposited for issue of a duplicate license.
+</li><li>In case of change in the constitution of the firm notarized and self attested copies of the new partnership deed must be submitted.
+</li><li>In case of change in the Board of Directors of a private or public limited company, the attested copy of Board Resolutions, Form No.32 submitted to the Registrar of Companies, and latest list of board of directors must be submitted, with then DIN documents.
+</li><li>There are provisions for renewal of the factory license for a period up to five years.
+</li><li>If a factory license has been renewed for a period for more than one year and it becomes necessary to get the license amended on account of any change/increase in the number of worker or power, the difference amount as per fee schedule shall be payable.
+</li><li>In case of loss or misplacement of the factory license, duplicate factory license shall be issued on payment of Rs. 100/- after an application in the prescribed Performa has been received.
+<li>
+</ol>
+
+</div>
+	
+</body>
+</html>
