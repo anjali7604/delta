4984b24243748013522d9aaa477c8af3ac1bc4db
anjali7604 <anjaliandng@gmail.com>
2015-07-16 19:52:31 +0530

    Update survey.php

--- survey.php
+++ survey.php
@@ -21,6 +21,52 @@ font-size:60px;
 margin-top:80px;
 
 }
+
+#sideimg{margin-left:70%;
+margin-top:0%;}
+
+a{
+    width: 150px;
+    height: 50px;
+    
+    -webkit-transition: width 2s, height 4s; /* For Safari 3.1 to 6.0 */
+    transition: width 2s, height 4s;
+}
+
+a:hover {
+    width: 170px;
+    height: 70px;
+	transform: rotate(20deg);
+}
+
+
+#anim {
+    width: 1000px;
+    height: 30px;
+    background-color: red;
+	padding:10px;
+    position: relative;
+    -webkit-animation-name: example; /* Chrome, Safari, Opera */
+    -webkit-animation-duration: 4s; /* Chrome, Safari, Opera */
+    -webkit-animation-iteration-count: infinite; /* Chrome, Safari, Opera */
+    animation-name: example;
+    animation-duration: 15s;
+    animation-iteration-count: infinite;
+}
+
+/* Chrome, Safari, Opera */
+@-webkit-keyframes example {
+    0%   {background-color:red; left:0px; top:0px;}
+    100%  {background-color:yellow; left:900px; top:0px;}
+    
+}
+
+/* Standard syntax */
+@keyframes example {
+    0%   {background-color:red; left:0px; top:0px;}
+    100%  {background-color:yellow; left:900px; top:0px;}
+    
+}
  </style>
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link href="theme/css/bootstrap.min.css" rel="stylesheet">
@@ -44,6 +90,10 @@ margin-top:80px;
  
  <h1 id="header">FACTORY INSPECTION DEPARTMENT</h1><br><br><br><br><br><br><br><br><br><br><br>
  </div>
+ 
+ <div id="anim">welcome to inspection department.we are here to create tranperency to common man about the factories status whose products they are consuming </div>
+ 
+ 
  <div id="page">
  <ul class="nav navbar-nav">
  <li><a href="home.php">Home</a></li>
@@ -86,6 +136,7 @@ margin-top:80px;
 <input type="submit">
 </form>
 
+<br><br><br><br> <br><img id="sideimg"src="flag.jpg">
 
 
 </body>
