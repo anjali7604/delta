8d926d0f214bd8493f39bf8e820141fdbf56bf05
anjali7604 <anjaliandng@gmail.com>
2015-07-16 14:36:21 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Factory Inspection Department</title>
+ 
+ <style>
+ #home{float:left;
+ margin-left:30%;
+ margin-top:5%;}
+ #obj{
+ margin-left:5%;
+ color:green;
+ font-size:20px;}
+ 
+ #image{float:left;
+ margin-left:75%;
+ margin-top:1%;}
+ 
+ #top{background-color: #b0c4de;}
+ 
+ #logo,#page{float:left;}
+ 
+ #header{
+float:left;
+font-size:60px;
+margin-top:80px;
+
+}
+ </style>
+ 
+ 
+ 
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="theme/css/bootstrap.min.css" rel="stylesheet">
+ <link href="theme/css/sb-admin-2.css" rel="stylesheet">
+ <script src="theme/js/html5shiv.js"></script>
+ <script src="theme/js/respond.min.js"></script>
+ <script src="theme/js/jquery-1.11.0.js"></script>
+ </head>
+
+
+
+
+
+
+
+
+<body>
+<nav class="navbar navbar-inverse" role="navigation">
+ <div id="top"><img src="logo.png" id="logo">
+ 
+ <h1 id="header">FACTORY INSPECTION DEPARTMENT</h1><br><br><br><br><br><br><br><br><br><br><br>
+ </div>
+ <div id="page">
+ <ul class="nav navbar-nav">
+ <li class="active"><a href="home.php">Home</a></li>
+ <li><a href="acts.php">Policies and Acts</a></li>
+ <li><a href="survey.php">Survey reports</a></li>
+ <li><a href="contactus.php">Contact Us</a></li>
+ </ul>
+ </li>
+ </ul>
+ </div>
+ </nav>
+ <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+
+ <li>
+<a href="authorised.php"><i class="fa fa-dashboard fa-fw"></i> Authorised Factories</a>
+</li>
+<li>
+<a href="unauthorised.php"><i class="fa fa-dashboard fa-fw"></i> Unauthorised Factories</a>
+</li>
+<li>
+<a href="faq.php"><i class="fa fa-dashboard fa-fw"></i> FAQ</a>
+</li>
+
+            </ul>
+        </div>
+      </div>
+	  
+	  <div id="home">
+	  
+	 <div id="obj"> To strive to implement statutory
+provisions under the factories act,<br>
+1948 effectively in the factories 
+& guiding and promoting the <br>
+Entrepreneurs in the matter of 
+INDUSTRIAL Safety & HEALTH Standards<br>
+in the Industries and the Community<br>
+</div>
+	  
+	  <br><br>
+	  
+	  
+	  
+<ul>	  
+	  <li>Established in 1971.</li>
+<li>To ensure safety, health and welfare of the factory workers.</li>
+<li>Enforcement of The Factories Act, 1948 and Rules made there under .</li>
+<li>Registration of Factories.</li>
+<li>Approval of factory building plans(drawings).</li>
+<li>To guide Major Accident Hazard units from chemical safety point of view.</li>
+<li>Investigation of accidents and dangerous occurrences in factories.Inspection of Factories.</li>
+
+<li>Enforcement of Payment of Wages Act, 1936 and Rules made there under .</li>
+<li>Imparting training to industrial workers, supervisors and engineers at safety museum and training centre</li>
+<li>To collect and analyze samples taken from working environment of factories.</li>
+<li>Enforcement of safety and health provisions of the Building and Other Construction Workers' (Regulation<br> of Employment and Other Conditions of Service) Act, 1996</li>
+</ul>	  
+	  </div>
+  
+</body>
+</html>
